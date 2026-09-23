4b216674f4b129fbe0ad237dc7bc9e60d83d3e92
Himanshu Kumar <himanshukumar53111@gmail.com>
2026-09-23 21:49:50 +0530

    Refactor climb_stairs method implementation
    Refactor climb_stairs method to use a list for dynamic programming.

--- Climb stairs.py
+++ Climb stairs.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    @staticmethod
-    def climb_stairs(n: int) -> int:
-        if n <= 2:
-            return n  # ways(1) = 1, ways(2) = 2
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
 
-        prev2, prev1 = 1, 2  # ways(1), ways(2)
-        for _ in range(3, n + 1):
-            prev2, prev1 = prev1, prev1 + prev2
-        return prev1
+        a = [1, 2]
+        for i in range(2, n):
+            a.append(a[i-2] + a[i-1])
 
-
-# quick test
-for i in range(1, 8):
-    print(i, Solution.climb_stairs(i))
+        return a[-1]
